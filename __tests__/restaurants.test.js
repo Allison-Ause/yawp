78a1623d7e6b7c54eda47be2e1c1643327e55f00
Allison Ause <allison.ause@gmail.com>
2022-08-18 10:45:27 -0700

    removed erroneous console logs from merged stretch branch

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -52,7 +52,6 @@ describe('restaurant routes', () => {
     const res = await request(app).get(
       '/api/v1/restaurants/search?name=Tropicale'
     );
-    console.log('RES.BODY in TEST', res.body);
     expect(res.status).toBe(200);
     expect(res.body).toEqual({
       id: '1',
